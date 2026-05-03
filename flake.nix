acdf76c5b53348e78374fc36f21a4c3dbba7dc4e
James Barnfather <jamesbarnfather99@gmail.com>
2026-05-03 14:47:15 +0100

    Updated and removed some dependecies

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Boids simulation";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          devShells.default = (pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+            packages = with pkgs; [
+              pkg-config
+	            bear
+              lldb
+              clang-tools
+              valgrind
+              cmake
+
+              sdl3
+              sdl3-ttf
+              sdl3-image
+
+              assimp
+              freetype
+              lua
+            ];
+          });
+        }
+      );
+}
